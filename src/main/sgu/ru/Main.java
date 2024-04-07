69fefc353e7f8b555b699ef3e8ef312ab457c29a
Sergey Okunkov <makdaf007@yandex.ru>
2024-04-07 23:36:03 +0400

    Made some changes in main file

--- src/main/sgu/ru/Main.java
+++ src/main/sgu/ru/Main.java
@@ -1,39 +1,33 @@
 package main.sgu.ru;
 
-import java.util.Scanner;
-
 public class Main {
 
     public void chooseMode() {
-        Scanner scanner = new Scanner(System.in);
         System.out.println("Options:" +
                             "\n1. First task." +
                             "\n2. Second task." +
                             "\n3. Third task." +
                             "\n4. Exit.");
         System.out.print("Choose your option: ");
-        String bl = scanner.next();
-        scanner.nextLine();
-        if (bl.equals("1")) {
+        String num = System.console().readLine();
+        if (num.equals("1")) {
             A a = new A();
             a.main();
         }   
-        else if (bl.equals("2")) {
+        else if (num.equals("2")) {
             B b = new B();
             b.main();
         }
-        else if (bl.equals("3")) {
+        else if (num.equals("3")) {
             C c = new C();
             c.main();
         }
-        else if (bl.equals("4")) {
+        else if (num.equals("4")) {
             System.out.println("\nStopping program...");
         }
         else {
             System.out.println("\nInvalid input!");
-        }
-        scanner.close();
-    
+        } 
     }
 
     public static void main(String[] args) {
